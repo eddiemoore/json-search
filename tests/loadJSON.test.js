2e00ef28a263cb908d137980a9188aa722e5d889
Ed Moore <ed@90seconds.tv>
2018-08-31 01:12:23 +1000

    Update variable name

--- tests/loadJSON.test.js
+++ tests/loadJSON.test.js
@@ -1,15 +1,15 @@
-const loadJson = require('../lib/loadJSON')
+const loadJSON = require('../lib/loadJSON')
 
 describe('load json', () => {
   it('should load and parse a json file', () => {
     const expected = require('./fixtures/users.json')
-    const actual = loadJson('./tests/fixtures/users.json')
+    const actual = loadJSON('./tests/fixtures/users.json')
     expect(actual).toEqual(expected)
   })
 
   it("should throw an error if the file doesn't exist", done => {
     try {
-      loadJson('./tests/fixtures/random file.json')
+      loadJSON('./tests/fixtures/random file.json')
     } catch (error) {
       expect(error.toString()).toContain('no such file or directory')
       done()
